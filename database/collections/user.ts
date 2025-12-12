f3622faa0d4ec2f5f692c97eec73937b6f95572e
MBarker2BYU <mbarker2@byupathway.edu>
2025-12-12 01:32:13 -0500

    removed database layer

--- database/collections/user.ts
+++
@@ -1,8 +0,0 @@
-// /database/collections/users.ts
-
-import { getDatabase } from "../connect";
-
-export async function getUsersCollection() {
-  const db = await getDatabase();
-  return db.collection("users");
-}
--- database/index.ts
+++
@@ -1,9 +0,0 @@
-// /database/index.ts
-
-import { getDatabase } from "./connect";
-import { runSeedIfNeeded } from "./seed";
-
-export async function initializeDatabase() {
-  await getDatabase();
-  await runSeedIfNeeded();
-}
